aeaa6b86f4e1e57822e18a828afc0852887819c8
Misgana Bayetta <misgana.bayetta@gmail.com>
2013-09-27 09:49:09 +0300

    some modifications.

--- src/com/addisai/binarizer/Binarizer.java
+++ src/com/addisai/binarizer/Binarizer.java
@@ -7,215 +7,162 @@ package com.addisai.binarizer;
 import com.addisai.commons.data.CSVDataManager;
 import com.addisai.commons.helpers.Numerics;
 import com.addisai.commons.stat.DistributionStat;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  *
  * @author Misgana Bayetta <misgana.bayetta@gmail.com>
- */
-public class Binarizer {
+ */public class Binarizer {
 
-    /**
-     * @param args the command line arguments
-     */
     CSVDataManager dm;
-    DayPopularity dp;
-    String[] featureNames;
     ArrayList<String[]> rawData;
-    ArrayList<String[]> binarizedData;
-    int[] MA_WINSIZEs = {5, 10, 20, 30, 50}; //different set of moving average window sizes
+    final int[] M_WINSIZEs = {5, 10, 20, 30, 50}; //different set of moving average window sizes
     int lookAheadDay; //K
     int lookBackDay; //R   
-    int Q_binSize;  //the binsize for the R days lookback moving averages
+    final int Q_binSize = 5;  //the binsize for the R days lookback moving averages
     boolean catagorizationIs_UP;
-    Date D;
+    final String CSV_Dir="song_bin";
+    File f;
+    private String[] featureNames;
+   
+    enum Category {
 
-    /**
-     * Initializes the binarization params
-     *
-     * @param fileName
-     * @param lookAhead
-     * @param getlookBackList
-     * @param date
-     */
-    public Binarizer(String fileName, int lookAhead, int lookBack, Date date, int Q_binSize) {
+        UP, NEUTRAL, DOWN //set values for each
+    };
+
+    public Binarizer(String rawDataFilePath, int lookBackDays, int lookAheadDays, boolean catType) {
         try {
-            dm = new CSVDataManager(fileName);
-            rawData = (ArrayList<String[]>) dm.getData();
-            this.lookAheadDay = lookAhead;
-            lookBackDay = lookBack;
-            D = date;
-            this.Q_binSize = Q_binSize;
-            dp = new DayPopularity(date, rawData);
-            featureNames = new String[(Q_binSize * MA_WINSIZEs.length) + 1]; //the last one is for the target class
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            dm = new CSVDataManager(rawDataFilePath);
+            this.lookBackDay = lookBackDays;
+            this.lookAheadDay = lookAheadDays;
+            this.catagorizationIs_UP = catType; 
+           
+            //create a saving dir
+            StringTokenizer stk=new StringTokenizer(CSV_Dir,"/");
+            String fileName=null;
+            while(stk.hasMoreElements()){
+                fileName=stk.nextToken();
+            }
+            f=new File(CSV_Dir+"_of_"+fileName);
+            f.mkdir();
+            
+        } catch (FileNotFoundException ex) {
             Logger.getLogger(Binarizer.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
-    /**
-     * Sets the T_UP and T_DOWN values
-     *
-     * @param lookBacks
-     */
-    private double upThreshold(double[] lookBacks) { //this method currently throws exception for set size less than three
-        double[] temp = lookBacks.clone();
-        /*Arrays.sort(temp);
-         int bottomBinSize=(temp.length/3); 
-         T_DOWN=temp[bottomBinSize];
-         T_UP=temp[bottomBinSize+(temp.length-bottomBinSize*2)-1];*/
-        List< Double[]> thirdQuantile = DistributionStat.getBuckets(lookBacks, 3);
-        return thirdQuantile.get(1)[thirdQuantile.get(1).length - 1];
+    private Category getCat(Double[] lookBackValues, Double val) {
+        List<Double[]> bins = DistributionStat.getBuckets(lookBackValues, 3);
+        Double[] midBucket = bins.get(1);
+        Category cat = null;
+        if (midBucket[0] > val) {
+            cat = Category.DOWN;
+        } else if (midBucket[0] <= val && midBucket[midBucket.length - 1] >= val) {
+            cat = Category.NEUTRAL;
+        } else if (midBucket[midBucket.length - 1] < val) {
+            cat = Category.UP;
+        }
+        return cat;
     }
 
-    private double downThreshold(double[] lookBacks) { //this method currently throws exception for set size less than three
-        double[] temp = lookBacks.clone();
-        /*Arrays.sort(temp);
-         int bottomBinSize=(temp.length/3); 
-         T_DOWN=temp[bottomBinSize];
-         T_UP=temp[bottomBinSize+(temp.length-bottomBinSize*2)-1];*/
-        List< Double[]> thirdQuantile = DistributionStat.getBuckets(lookBacks, 3);
-        return thirdQuantile.get(1)[0];
-
+    private void setCatValue(String[] rowBinVlaues, Category cat) {
+        if (catagorizationIs_UP) {
+            if (cat == Category.UP) {
+                rowBinVlaues[rowBinVlaues.length - 1] = "1";
+            } else {
+                rowBinVlaues[rowBinVlaues.length - 1] = "0";
+            }
+        }
+        if (!catagorizationIs_UP) {
+            if (cat == Category.DOWN) {
+                rowBinVlaues[rowBinVlaues.length - 1] = "1";
+            } else {
+                rowBinVlaues[rowBinVlaues.length - 1] = "0";
+            }
+        }
     }
 
-    public List<String[]> getBinData(String datasetFileName) {
-        rawData = (ArrayList<String[]>) dm.getData();
-        ArrayList<SongPopularityOnDay> sodList = dp.getAllSongPopularitOnDay();//THIS LINE HAS A DEFFECT
-        HashMap<String, Double[]> songLookBacks = new HashMap<>();   // musicId key and popularity set values     
-        int[] binValues = new int[Q_binSize * featureNames.length + 1]; //last one for UP/DOWN
-        int UP;
-        int DOWN;
-        List<String[]> binerizedDataSet = new ArrayList<>();
-        //set feature names
-        for (int i = 0; i < Q_binSize; i++) {
-            for (int j = 0; j < MA_WINSIZEs.length; j++) {
-                featureNames[i] = "MA_" + j + "_bin" + i;
+    private void setFeatureNames() {
+        featureNames = new String[2 * Q_binSize * M_WINSIZEs.length + 1]; // The last one is for the target class
+        for (int l = 0; l < M_WINSIZEs.length; l++) {
+            for (int i = 0; i < Q_binSize * M_WINSIZEs.length; i++) {
+                featureNames[i] = "MA" + M_WINSIZEs[l] + "_bin" + i;
+            }
+        }
+        for (int l = 0; l < M_WINSIZEs.length; l++) {
+            for (int i = Q_binSize * M_WINSIZEs.length; i < featureNames.length - 1; i++) {
+                featureNames[i] = "MV" + M_WINSIZEs[l] + "_bin" + i;
             }
         }
         featureNames[featureNames.length - 1] = "OUT";
-        //get all popularity values related to a songId and add it to a list
-        for (SongPopularityOnDay sod : sodList) {
-            try {
-                songLookBacks.put(sod.songId, getlookBackList(sod.songId, lookBackDay));//get R lookback popularity of each song
+    }
 
-            } catch (Exception ex) {
-                ex.getMessage();
-                Logger.getLogger(Binarizer.class.getName()).log(Level.SEVERE, null, ex);
+    private HashMap<String, Double[]> getSongPopularityhash(List<String[]> headerRemovedData) {
+        HashMap<String, Double[]> hashedList = new HashMap<>();
+        for (String[] st : headerRemovedData) {
+            String musicId = st[0];
+            Double[] popularity = new Double[st.length - 1];
+            for (int i = 1; i < st.length; i++) {
+                popularity[i - 1] = Double.valueOf(st[i]);
             }
+            hashedList.put(musicId, popularity);
         }
-        //apply moving average on each popualrity values of each song in the list with d/t window sizes
-        List<HashMap<Integer, Double[]>> Q_MAList = new ArrayList<>(); //for containing d/t window size moving averages
-        for (String key : songLookBacks.keySet()) {
-            Double[] lookBacks = songLookBacks.get(key);
-            //set the UP,NEUTRAL and down 
-            //CAUTION adjust it with D+K days popularity
-            if (lookBacks[lookBacks.length - 1] <= downThreshold(Numerics.doubleObjToDoublePrim(lookBacks))) {
-                if (catagorizationIs_UP) {
-                    UP = DOWN = 0;
-                } else {
-                    DOWN = 1;
-                    UP = 0;
+        return hashedList;
+    }
+
+    public void binarize() {
+        ArrayList<String[]> headerRemovedData = (ArrayList<String[]>) rawData.clone();
+        headerRemovedData.remove(0);
+        setFeatureNames();
+        HashMap<String, Double[]> songPopularityhash = getSongPopularityhash(headerRemovedData);
+        String fileName;  //the file name for the binary
+        List<String[]> binData = new ArrayList<>(); //the final output to be saved
+        binData.add(featureNames);
+        System.out.println("INFO:Started Binarization of");
+        for (String key : songPopularityhash.keySet()) { //loop on each song
+            System.out.print("music #"+key+"...");
+            fileName = key + ".moses";           
+            Double[] songPoplarity = songPopularityhash.get(key);
+            for (int i = lookBackDay; i < songPoplarity.length; i++) {
+                Double[] lookBackData = Numerics.getLookBacks(songPoplarity, i, lookBackDay);
+                Category prCat = getCat(lookBackData, songPoplarity[i]); //Modify it with the look ahead value
+                String[] rowBinVlaues = new String[featureNames.length]; //the bin values as a string 
+                setCatValue(rowBinVlaues, prCat);
+                int ref = 0;
+                for (int j = 0; j < M_WINSIZEs.length; j++) { //binarize using d/t window sizes with Moving average and Moving variance
+                    Double[] lookBackMA = DistributionStat.getMvoingAverage(lookBackData, M_WINSIZEs[j]);
+                    Double[] lookBackMV = DistributionStat.getMovingVariance(lookBackData, M_WINSIZEs[j]);
+                    List<Double[]> lookBackMABucket = DistributionStat.getBuckets(lookBackMA, Q_binSize);
+                    List<Double[]> lookBackMVBucket = DistributionStat.getBuckets(lookBackMV, Q_binSize);
+
+
+                    //Moving averages binarization
+                    Integer[] MABinValue = Numerics.binarize(lookBackMABucket);
+                    for (int index = 0; index < MABinValue.length; index++) {
+                        rowBinVlaues[ref] = String.valueOf(MABinValue[index]);
+                        ref++;
+                    }
+                    //Moving variance binarization
+                    Integer[] MVBinValue = Numerics.binarize(lookBackMVBucket);
+                    for (int index = 0; index < MVBinValue.length; index++) {
+                        rowBinVlaues[ref] = String.valueOf(MVBinValue[index]);
+                        ref++;
+                    }
                 }
-            } else if (lookBacks[lookBacks.length - 1] >= upThreshold(Numerics.doubleObjToDoublePrim(lookBacks))) {
-                if (catagorizationIs_UP) {
-                    UP = 1;
-                    DOWN = 0;
-                } else {
-                    UP = DOWN = 0;
-                }
-            } else {  //neutral case
-                UP = DOWN = 0;
+               binData.add(rowBinVlaues);
             }
-            // calculate the R days moving average with d/t windows of D of each song i.e  (D-R+1,D) 
-            HashMap<Integer, Double[]> Q_MAs = new HashMap<>(); //key is the window size and values are the moving average of the song
-
-            for (int maIndex = 0; maIndex < MA_WINSIZEs.length; maIndex++) {
-                double[] songMAs = new double[songLookBacks.get(key).length];
-                for (int index = songLookBacks.get(key).length - 1; index >= 0; index--) {
-                    songMAs[index] = DistributionStat.getMvoingAverage(songLookBacks.get(key), MA_WINSIZEs[maIndex]);
-                }
-                Q_MAs.put(MA_WINSIZEs[maIndex], Numerics.doublePrimToDoubleObj(songMAs));
-            }
-            Q_MAList.add(Q_MAs);
-
+           //save the CSV generated
+            System.out.print("saving("+fileName+")...");
+           dm.saveCSV(binData,f.getAbsolutePath()+"/"+fileName);
+            System.out.println("finished");
         }
-        //binerize          
-        return null;
     }
-    /**
-     * 
-     * @param maData
-     * @param valBinarized
-     * @return 
-     */     
-    private int[] getBinVal(Double[] maData,int binSize, Double valBinarized) {       
-        List<Double[]> maBins=DistributionStat.getBuckets(Numerics.doubleObjToDoublePrim(maData), binSize);
-        int [] bits=new int[binSize];
-        for(Double [] d:maBins){
-            int i=0;
-            if(valBinarized)
-        }        
-    }
-
-    /**
-     *
-     * @param songId
-     * @param R
-     * @return The R days look backs and throws exception if the dataset size is
-     * less than or equal to R
-     * @throws Exception
-     */
-    public Double[] getlookBackList(String songId, int R) throws Exception {
-        Double[] lookBacks;
-        if (dm.getDataWidth() < R) {
-            throw new Exception("Look back size should be smaller than dataset size");
-        } else {
-            lookBacks = dp.getInBetweenPopularity(songId, dp.getDateIndex() - (R - 1), dp.getDateIndex());
-
-        }
-        return lookBacks;
-    }
-
-    public int getLookAheadDay() {
-        return lookAheadDay;
-    }
-
-    public int getLookBackDay() {
-        return lookBackDay;
-    }
-
-    public Date getD() {
-        return D;
-    }
-//    public static void main(String[] args) {
-//        /*PARAMS
-//         -R (The look back)
-//         -K (The look ahead)
-//         -D (The day)
-//         *PROCEDURES
-    //        -Set UP and DOWN
-//         -Get all songs of the day
-//         -calculate the thresholds with R days look bakc
-//         -calculate the moving averages of 5,10,20,30 and 50 days
-//         -quantize the moving averages
-//         ******/
-//        Binarizer b = new Binarizer(null, 0, 0, null);
-//        double[] thTest = {4820, 5041, 5580, 5568, 5973};
-//        b.upThreshold(thTest);
-//        System.out.print("Input:{");
-//        for (double d : thTest) {
-//            System.out.print(d + ",");
-//        }
-//        System.out.println("}");
-//        System.out.println("T_DOWN:" + b.getT_DOWN());
-//        System.out.println("T_UP:" + b.getT_UP());
-//    }
+   
 }
